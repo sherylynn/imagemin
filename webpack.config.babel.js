d4ac58afdc830d1a8f5184123187cc26485fb1be
sherylynn <352281674@qq.com>
2017-09-25 23:56:25 +0800

    更新了脚手架

---
+++ webpack.config.babel.js
@@ -0,0 +1,45 @@
+import webpack from 'webpack';
+import fs from 'fs';
+import path from 'path';
+const nodeExternals = require('webpack-node-externals');
+
+export default {
+    cache: true,
+    entry: [
+        'webpack/hot/poll?1000',
+        './src/index.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    context: __dirname,
+    node: {
+        __filename: false,
+        __dirname: false
+    },
+    target: 'node',
+    externals: [nodeExternals({
+      whitelist: ['webpack/hot/poll?1000', 'parse']
+    })],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: [
+                path.resolve(__dirname, "node_modules"),
+            ],
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        //new webpack.NoEmitOnErrorsPlugin()
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.json']
+    }
+}
