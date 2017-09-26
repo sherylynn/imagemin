4aec0e0dddf2e754e1e08b51a2acb5c57f075d54
sherylynn <352281674@qq.com>
2017-09-27 03:09:26 +0800

    增加了socket.io的包,fix了tsconfig的配置文件

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,14 @@
 const Koa=require('koa')
 const app= new Koa()
+/*
+
+*/
+let server= require('http').createServer(app.callback())
+let io=require('socket.io')(server)
+io.on('connection',function(){
+
+})
 app.use((ctx:any)=>{
   ctx.body='Hello Koa'
 })
-app.listen(3000)
+server.listen(3000)
