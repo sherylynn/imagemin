280d3601f57d184c3d266d6ec7abc632136646f8
sherylynn <352281674@qq.com>
2017-11-16 11:06:41 +0800

    添加了隐藏黑框

--- 添加右击图片压缩.bat
+++ 添加右击图片压缩.bat
@@ -4,8 +4,11 @@ cd /d "%~dp0"
 @echo off
 cls
 setlocal EnableDelayedExpansion
-Reg add HKCR\*\shell\imagemin /ve /t REG_SZ /d "缩小图片" /f
+Reg add HKCR\*\shell\imagemin /ve /t REG_SZ /d "压缩图片" /f
 Reg add HKCR\*\shell\imagemin /v "Icon" /t REG_SZ /d "%~dp0icon_small.ico" /f
-Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "%~dp0imagemin.bat \"%%1\"" /f
+rem 这种是不显示进度条黑框
+Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~dp0imagemin.bat %%1\",\"\",\"\",0)(window.close)"  /f
+rem 这种是显示
+echo Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "%~dp0imagemin.bat \"%%1\"" /f
 
 pause
