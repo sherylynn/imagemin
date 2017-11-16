8d0b62cb602c73e93ac972b15ddfa01443510342
sherylynn <352281674@qq.com>
2017-11-16 11:13:46 +0800

    添加了显示

--- 添加右击图片压缩.bat
+++ 添加右击图片压缩.bat
@@ -4,11 +4,13 @@ cd /d "%~dp0"
 @echo off
 cls
 setlocal EnableDelayedExpansion
+Reg add HKCR\*\shell\imagemin_hide /ve /t REG_SZ /d "后台压缩图片" /f
+Reg add HKCR\*\shell\imagemin_hide /v "Icon" /t REG_SZ /d "%~dp0icon_small.ico" /f
+Reg add HKCR\*\shell\imagemin_hide\command /ve /t REG_SZ /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~dp0imagemin.bat %%1\",\"\",\"\",0)(window.close)"  /f
+
+
 Reg add HKCR\*\shell\imagemin /ve /t REG_SZ /d "压缩图片" /f
 Reg add HKCR\*\shell\imagemin /v "Icon" /t REG_SZ /d "%~dp0icon_small.ico" /f
-rem 这种是不显示进度条黑框
-Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"cmd.exe\",\"/c %~dp0imagemin.bat %%1\",\"\",\"\",0)(window.close)"  /f
-rem 这种是显示
-echo Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "%~dp0imagemin.bat \"%%1\"" /f
+Reg add HKCR\*\shell\imagemin\command /ve /t REG_SZ /d "%~dp0imagemin.bat \"%%1\"" /f
 
 pause
